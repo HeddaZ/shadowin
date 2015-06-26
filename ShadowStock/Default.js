9b1a34c331fb9ffc29aa0e862dd971263e0a407a
Hedda Zhang <heddaz@live.com>
2015-06-26 23:30:40 +0800

    Update the UI and compatibility.

--- ShadowStock/Default.js
+++ ShadowStock/Default.js
@@ -1,7 +1,7 @@
 ﻿/* ******************* 全局变量 ******************* */
 
 //常量
-var MarkedCellKeysStringCookieName="MarkedCellKeysString";
+var MarkedCellKeysStringCookieName = "MarkedCellKeysString";
 //控件
 var LockButton;
 var StocksString;
@@ -15,163 +15,136 @@ var StockQuantitys;
 
 /* ******************* 事件响应 ******************* */
 
-function Page_Load(sender, e)
-{
+function Page_Load(sender, e) {
     //控件定义
-    LockButton=document.getElementById("LockButton");
-    StocksString=document.getElementById("StocksString");
-    ContentContainer=document.getElementById("ContentContainer");
-    StockDatasFetcherContainer=document.getElementById("StockDatasFetcherContainer");
-    
+    LockButton = document.getElementById("LockButton");
+    StocksString = document.getElementById("StocksString");
+    ContentContainer = document.getElementById("ContentContainer");
+    StockDatasFetcherContainer = document.getElementById("StockDatasFetcherContainer");
+
     //股票清单字符串
-    var cookieStocksStringValue=ReadCookie(StocksString.id);
-    if (cookieStocksStringValue)
-    {
-        StocksString.value=cookieStocksStringValue;
-        StocksString.disabled=LockButton.checked=true;
+    var cookieStocksStringValue = ReadCookie(StocksString.id);
+    if (cookieStocksStringValue) {
+        StocksString.value = cookieStocksStringValue;
+        StocksString.disabled = LockButton.checked = true;
     }
-    else
-    {
-        StocksString.value="sh000001,sz000002";
-        StocksString.disabled=LockButton.checked=false;
+    else {
+        StocksString.value = "sh000001,sz000002";
+        StocksString.disabled = LockButton.checked = false;
     }
-    
+
     //标题
     ContentContainer.deleteTHead();
-    var contentContainerHeader=ContentContainer.createTHead();
+    var contentContainerHeader = ContentContainer.createTHead();
     CreateRow(contentContainerHeader, HeadersString.split(","), DisplayIndexsString.split(","));
-    
+
     //第一次启动刷新器
     Refresher_Elapsed();
 }
 
-function StocksString_KeyPress(sender, e)
-{
+function StocksString_KeyPress(sender, e) {
     return /[shzSHZ0-9\/\.,]/.test(String.fromCharCode(event.keyCode));
 }
-function StocksString_Blur(sender, e)
-{
+function StocksString_Blur(sender, e) {
     //规整化
-    sender.value=sender.value.toLowerCase().replace(/\s+/g, "").replace(/,{2,}/g, ",").trim(",");
-    if (sender.value=="")
-    {
-        var cookieStocksStringValue=ReadCookie(sender.id);
-        if (cookieStocksStringValue)
-        {
-            sender.value=cookieStocksStringValue;
+    sender.value = sender.value.toLowerCase().replace(/\s+/g, "").replace(/,{2,}/g, ",").trim(",");
+    if (sender.value == "") {
+        var cookieStocksStringValue = ReadCookie(sender.id);
+        if (cookieStocksStringValue) {
+            sender.value = cookieStocksStringValue;
         }
     }
-    if (sender.value && !/^[s][shz0-9\/\.,]*[0-9\/]$/g.test(sender.value))
-    {
+    if (sender.value && !/^[s][shz0-9\/\.,]*[0-9\/]$/g.test(sender.value)) {
         alert("选股列表异常！\n\n【格式】代码[/成本[/持有量]][,代码[/成本[/持有量]]]\n【例如】sh600026,sh600050/2.33,sz000002/20.5/100");
         sender.focus();
         sender.select();
     }
 }
 
-function LockButton_Click(sender, e)
-{
-    if (sender.checked)
-    {
+function LockButton_Click(sender, e) {
+    if (sender.checked) {
         //保存到Cookie
         WriteCookie(StocksString.id, StocksString.value);
     }
-    StocksString.disabled=sender.checked;
+    StocksString.disabled = sender.checked;
 }
 
 //刷新器事件
-function Refresher_Elapsed(e)
-{
+function Refresher_Elapsed(e) {
     //关闭刷新器
-    if (Refresher)
-    {
-        Refresher=window.clearTimeout(Refresher);
+    if (Refresher) {
+        Refresher = window.clearTimeout(Refresher);
     }
 
     //未操作
-    if (LockButton.checked)
-    {
+    if (LockButton.checked) {
         //分析选股列表
-        var stocks=StocksString.value.split(",");
-        StockCodes=new Array();
-        StockCosts=new Array();
-        StockQuantitys=new Array();
-        for (var i=0;i<stocks.length;i++)
-        {
-            var stockInformations=stocks[i].split("/");
-            if (stockInformations[0])
-            {
+        var stocks = StocksString.value.split(",");
+        StockCodes = new Array();
+        StockCosts = new Array();
+        StockQuantitys = new Array();
+        for (var i = 0; i < stocks.length; i++) {
+            var stockInformations = stocks[i].split("/");
+            if (stockInformations[0]) {
                 StockCodes.push(stockInformations[0]);
-                if (stockInformations[1] && !isNaN(stockInformations[1]))
-                {
+                if (stockInformations[1] && !isNaN(stockInformations[1])) {
                     StockCosts.push(stockInformations[1]);
                 }
-                else
-                {
+                else {
                     StockCosts.push(0);
                 }
-                if (stockInformations[2] && !isNaN(stockInformations[2]))
-                {
+                if (stockInformations[2] && !isNaN(stockInformations[2])) {
                     StockQuantitys.push(stockInformations[2]);
                 }
-                else
-                {
+                else {
                     StockQuantitys.push(0);
                 }
             }
         }
-        
+
         //启动股票数据取回器
-        StockDatasFetcherContainer.src=FormatString(StockDatasFetcherUrlFormat,
+        StockDatasFetcherContainer.src = FormatString(StockDatasFetcherUrlFormat,
             GenerateTimeID(), //避免缓存
             escape(StockCodes.join(","))
             );
-            
+
         //回调时将启动刷新器StockDatasFetcher_Callback
     }
-    else
-    {
+    else {
         //启动刷新器
-        Refresher=window.setTimeout(Refresher_Elapsed, RefreshInterval);
+        Refresher = window.setTimeout(Refresher_Elapsed, RefreshInterval);
     }
 }
 
 //单元格点击
-function Cell_DoubleClick(e)
-{
+function Cell_DoubleClick(e) {
     //本单元格关键字
-    var cellKey=this.parentElement.rowIndex+"/"+this.cellIndex;
-    
+    var cellKey = this.parentElement.rowIndex + "/" + this.cellIndex;
+
     //在已标记列表中查找本单元格关键字
     var cellKeyMarkedIndex;
     var markedCellKeys;
-    var markedCellKeysString=ReadCookie(MarkedCellKeysStringCookieName); //格式：行索引/列索引[,行索引/列索引]  例如：1/3,2/5,3/4,12/5
-    if (markedCellKeysString)
-    {
-        markedCellKeys=markedCellKeysString.split(",");
-        for (var i=0;i<markedCellKeys.length;i++)
-        {
-            if (markedCellKeys[i].trim()==cellKey)
-            {
-                cellKeyMarkedIndex=i;
+    var markedCellKeysString = ReadCookie(MarkedCellKeysStringCookieName); //格式：行索引/列索引[,行索引/列索引]  例如：1/3,2/5,3/4,12/5
+    if (markedCellKeysString) {
+        markedCellKeys = markedCellKeysString.split(",");
+        for (var i = 0; i < markedCellKeys.length; i++) {
+            if (markedCellKeys[i].trim() == cellKey) {
+                cellKeyMarkedIndex = i;
                 break;
             }
         }
     }
-    else
-    {
-        markedCellKeys=new Array();
+    else {
+        markedCellKeys = new Array();
     }
-    if (cellKeyMarkedIndex>=0)
-    {
+    if (cellKeyMarkedIndex >= 0) {
         //本单元格已被标记，取消标记
-        this.className="";
+        this.className = "";
         markedCellKeys.splice(cellKeyMarkedIndex, 1);
     }
-    else
-    {
+    else {
         //本单元格未被标记，标记
-        this.className="Marked";
+        this.className = "Marked";
         markedCellKeys.push(cellKey);
     }
     //更新已标记列表
@@ -180,67 +153,59 @@ function Cell_DoubleClick(e)
 
 /* ******************* 数据处理方法 ******************* */
 
-function StockDatasFetcher_Callback(stockDatasStrings)
-{
+function StockDatasFetcher_Callback(stockDatasStrings) {
     //内容（利用TFoot可批量删除的特性）
     ContentContainer.deleteTFoot();
-    var contentContainerBody=ContentContainer.createTFoot();
-    var displayIndexs=DisplayIndexsString.split(",");
-    for (var i=0;i<StockCodes.length;i++)
-    {
-        var stockDatas=stockDatasStrings[i].split(",");
-        
+    var contentContainerBody = ContentContainer.createTFoot();
+    var displayIndexs = DisplayIndexsString.split(",");
+    for (var i = 0; i < StockCodes.length; i++) {
+        var stockDatas = stockDatasStrings[i].split(",");
+
         EnrichStockDatas(stockDatas, StockCodes[i], StockCosts[i], StockQuantitys[i]);
         CreateRow(contentContainerBody, stockDatas, displayIndexs);
     }
-    
+
     //着色
     ColorRows(ContentContainer);
     //标记
     MarkCells(ContentContainer);
-    
+
     //启动刷新器
-    Refresher=window.setTimeout(Refresher_Elapsed, RefreshInterval);
+    Refresher = window.setTimeout(Refresher_Elapsed, RefreshInterval);
 }
 
 /* ******************* 数据处理方法 ******************* */
 
 //为容器创建数据行
-function CreateRow(container, datas, displayIndexs)
-{
-    var row=container.insertRow();
-    for (var i=0;i<displayIndexs.length;i++)
-    {
-        var cell=row.insertCell();
-        cell.innerHTML=datas[displayIndexs[i]];
-        cell.ondblclick=Cell_DoubleClick;
+function CreateRow(container, datas, displayIndexs) {
+    var row = container.insertRow();
+    for (var i = 0; i < displayIndexs.length; i++) {
+        var cell = row.insertCell();
+        cell.innerHTML = datas[displayIndexs[i]];
+        cell.ondblclick = Cell_DoubleClick;
     }
 }
 
 //扩充股票数据
-function EnrichStockDatas(datas, code, costPrice, quantity)
-{
+function EnrichStockDatas(datas, code, costPrice, quantity) {
     //0~32数来自SINA，后续需要进行计算扩充
-    var yestodayPrice=parseFloat(datas[2]);
-    var currentPrice=parseFloat(datas[3]);
-    if (currentPrice==0)
-    {
-        currentPrice=yestodayPrice;
+    var yestodayPrice = parseFloat(datas[2]);
+    var currentPrice = parseFloat(datas[3]);
+    if (currentPrice == 0) {
+        currentPrice = yestodayPrice;
     }
     //代码，去除SINA前缀
-    var plainCode=code.replace(/^s[hz]/g, "");
-    
+    var plainCode = code.replace(/^s[hz]/g, "");
+
     //8
-    if (code.startsWith("sh000"))
-    {
-        datas[8]=Math.round(parseFloat(datas[8])/10000)+"万";
+    if (code.startsWith("sh000")) {
+        datas[8] = Math.round(parseFloat(datas[8]) / 10000) + "万";
     }
-    else
-    {
-        datas[8]=Math.round(parseFloat(datas[8])/1000000)+"万";
+    else {
+        datas[8] = Math.round(parseFloat(datas[8]) / 1000000) + "万";
     }
     //9
-    datas[9]=Math.round(parseFloat(datas[9])/10000)+"万";
+    datas[9] = Math.round(parseFloat(datas[9]) / 10000) + "万";
 
     //预留
     for (var i = 1; i <= 8; i++) {
@@ -248,7 +213,7 @@ function EnrichStockDatas(datas, code, costPrice, quantity)
     }
 
     //41
-    var displayName=FormatString(StockDisplayNameFormat,
+    var displayName = FormatString(StockDisplayNameFormat,
         plainCode,
         code, //新浪兼容
         datas[0],
@@ -258,32 +223,32 @@ function EnrichStockDatas(datas, code, costPrice, quantity)
     //42
     datas.push(plainCode);
     //43
-    var currentDifferencePrice=Math.round((currentPrice-yestodayPrice)*100)*1.00/100;
+    var currentDifferencePrice = Math.round((currentPrice - yestodayPrice) * 100) * 1.00 / 100;
     datas.push(currentDifferencePrice);
     //44
-    var currentDifferenceRate=Math.round(currentDifferencePrice/yestodayPrice*10000)*1.00/100;
-    datas.push(currentDifferenceRate+"%");
+    var currentDifferenceRate = Math.round(currentDifferencePrice / yestodayPrice * 10000) * 1.00 / 100;
+    datas.push(currentDifferenceRate + "%");
     //45
     datas.push(costPrice);
     //46
-    var costDifferencePrice=(costPrice==0)?0:Math.round((currentPrice-costPrice)*100)*1.00/100;
+    var costDifferencePrice = (costPrice == 0) ? 0 : Math.round((currentPrice - costPrice) * 100) * 1.00 / 100;
     datas.push(costDifferencePrice);
     //47
-    var costDifferenceRate=(costPrice==0)?0:Math.round(costDifferencePrice/costPrice*10000)*1.00/100;
-    datas.push(costDifferenceRate+"%");
+    var costDifferenceRate = (costPrice == 0) ? 0 : Math.round(costDifferencePrice / costPrice * 10000) * 1.00 / 100;
+    datas.push(costDifferenceRate + "%");
     //48
     datas.push(quantity);
     //49
-    var currentAmount=Math.round(currentPrice*quantity*100)*1.00/100;
+    var currentAmount = Math.round(currentPrice * quantity * 100) * 1.00 / 100;
     datas.push(currentAmount);
     //50
-    var costAmount=Math.round(costPrice*quantity*100)*1.00/100;
+    var costAmount = Math.round(costPrice * quantity * 100) * 1.00 / 100;
     datas.push(costAmount);
     //51
-    var costDifferenceAmount=Math.round(costDifferencePrice*quantity*100)*1.00/100;
+    var costDifferenceAmount = Math.round(costDifferencePrice * quantity * 100) * 1.00 / 100;
     datas.push(costDifferenceAmount);
     //52
-    var link=FormatString(StockLinkFormat,
+    var link = FormatString(StockLinkFormat,
         plainCode,
         code //新浪兼容
         );
@@ -291,55 +256,43 @@ function EnrichStockDatas(datas, code, costPrice, quantity)
 }
 
 //着色
-function ColorRows(container)
-{
+function ColorRows(container) {
     //根据显示索引列表确定容器中的“着色基准”列
     var colorBaseContainerIndex;
-    var displayIndexs=DisplayIndexsString.split(",");
-    for (var i=0;i<displayIndexs.length;i++) 
-    {
-        if (parseInt(displayIndexs[i])==ColorBaseIndex)
-        {
-            colorBaseContainerIndex=i;
+    var displayIndexs = DisplayIndexsString.split(",");
+    for (var i = 0; i < displayIndexs.length; i++) {
+        if (parseInt(displayIndexs[i]) == ColorBaseIndex) {
+            colorBaseContainerIndex = i;
             break;
         }
     }
-    if (colorBaseContainerIndex>=0)
-    {
+    if (colorBaseContainerIndex >= 0) {
         //找到了“着色基准”列基于容器的索引，着色
-        for (var i=0;i<container.rows.length;i++)
-        {
-            var row=container.rows[i];
-            var colorBaseData=parseFloat(row.cells[colorBaseContainerIndex].innerText);
-            if (colorBaseData>0)
-            {
-                row.className="Plus";
+        for (var i = 0; i < container.rows.length; i++) {
+            var row = container.rows[i];
+            var colorBaseData = parseFloat(row.cells[colorBaseContainerIndex].innerText);
+            if (colorBaseData > 0) {
+                row.className = "Plus";
             }
-            else if (colorBaseData<0)
-            {
-                row.className="Minus";
+            else if (colorBaseData < 0) {
+                row.className = "Minus";
             }
         }
     }
 }
 
 //标记
-function MarkCells(container)
-{
-    var markedCellKeysString=ReadCookie(MarkedCellKeysStringCookieName); //格式：行索引/列索引[,行索引/列索引]  例如：1/3,2/5,3/4,12/5
-    if (markedCellKeysString)
-    {
-        var markedCellKeys=markedCellKeysString.split(",");
-        for (var i=0;i<markedCellKeys.length;i++)
-        {
-            var markedCellKeyInformations=markedCellKeys[i].split("/");
-            if (markedCellKeyInformations[0] && markedCellKeyInformations[1] && !isNaN(markedCellKeyInformations[0]) && !isNaN(markedCellKeyInformations[1]))
-            {
-                var markedRowIndex=parseInt(markedCellKeyInformations[0]);
-                var markedCellIndex=parseInt(markedCellKeyInformations[1]);
-                if (markedRowIndex<container.rows.length && markedCellIndex<container.rows[0].cells.length)
-                {
-                    container.rows[markedRowIndex].cells[markedCellIndex].className="Marked";
+function MarkCells(container) {
+    var markedCellKeysString = ReadCookie(MarkedCellKeysStringCookieName); //格式：行索引/列索引[,行索引/列索引]  例如：1/3,2/5,3/4,12/5
+    if (markedCellKeysString) {
+        var markedCellKeys = markedCellKeysString.split(",");
+        for (var i = 0; i < markedCellKeys.length; i++) {
+            var markedCellKeyInformations = markedCellKeys[i].split("/");
+            if (markedCellKeyInformations[0] && markedCellKeyInformations[1] && !isNaN(markedCellKeyInformations[0]) && !isNaN(markedCellKeyInformations[1])) {
+                var markedRowIndex = parseInt(markedCellKeyInformations[0]);
+                var markedCellIndex = parseInt(markedCellKeyInformations[1]);
+                if (markedRowIndex < container.rows.length && markedCellIndex < container.rows[0].cells.length) {
+                    container.rows[markedRowIndex].cells[markedCellIndex].className = "Marked";
                 }
             }
         }
--- ShadowStock/Shared.js
+++ ShadowStock/Shared.js
@@ -1,91 +1,71 @@
 ﻿/* ******************* 扩展原型 ******************* */
 
 //扩展原型
-String.prototype.trimStart=function(trimming)
-{
-    if (!trimming)
-    {
-        trimming="\\s";
+String.prototype.trimStart = function (trimming) {
+    if (!trimming) {
+        trimming = "\\s";
     }
-    var pattern=new RegExp("^("+trimming+")+", "g");
+    var pattern = new RegExp("^(" + trimming + ")+", "g");
     return this.replace(pattern, "");
 }
-String.prototype.trimEnd=function(trimming)
-{
-    if (!trimming)
-    {
-        trimming="\\s";
+String.prototype.trimEnd = function (trimming) {
+    if (!trimming) {
+        trimming = "\\s";
     }
-    var pattern=new RegExp("("+trimming+")+$", "g");
+    var pattern = new RegExp("(" + trimming + ")+$", "g");
     return this.replace(pattern, "");
 }
-String.prototype.trim=function(trimming)
-{
+String.prototype.trim = function (trimming) {
     return this.trimStart(trimming).trimEnd(trimming);
 }
 
-String.prototype.padLeft=function(width, padding)
-{
-    var result=this;
-    if (!padding)
-    {
-        padding=" ";
+String.prototype.padLeft = function (width, padding) {
+    var result = this;
+    if (!padding) {
+        padding = " ";
     }
-    while (result.length<width)
-    {
-        result=padding+result;
+    while (result.length < width) {
+        result = padding + result;
     }
     return result;
 }
-String.prototype.padRight=function(width, padding)
-{
-    var result=this;
-    if (!padding)
-    {
-        padding=" ";
+String.prototype.padRight = function (width, padding) {
+    var result = this;
+    if (!padding) {
+        padding = " ";
     }
-    while (result.length<width)
-    {
-        result=result+padding;
+    while (result.length < width) {
+        result = result + padding;
     }
     return result;
 }
 
-String.prototype.startsWith=function(string)
-{
-    if (!string)
-    {
-        string="\\s";
+String.prototype.startsWith = function (string) {
+    if (!string) {
+        string = "\\s";
     }
-    var pattern=new RegExp("^("+string+")", "g");
+    var pattern = new RegExp("^(" + string + ")", "g");
     return pattern.test(this);
 }
-String.prototype.endsWith=function(string)
-{
-    if (!string)
-    {
-        string="\\s";
+String.prototype.endsWith = function (string) {
+    if (!string) {
+        string = "\\s";
     }
-    var pattern=new RegExp("("+string+")$", "g");
+    var pattern = new RegExp("(" + string + ")$", "g");
     return pattern.test(this);
 }
 
 /* ******************* 名字值集合（name1=value1,name2=value2,name3=value3） ******************* */
 
-function GetNameValueCollectionValue(nameValueCollection, name, valueEscaped)
-{
-    for (var i = 0; i < nameValueCollection.length; i++)
-    {
+function GetNameValueCollectionValue(nameValueCollection, name, valueEscaped) {
+    for (var i = 0; i < nameValueCollection.length; i++) {
         var collectionName = nameValueCollection[i].substring(0, nameValueCollection[i].indexOf("=")).trim();
         var collectionValue = nameValueCollection[i].substr(nameValueCollection[i].indexOf("=") + 1);
-        if (collectionName == name.trim())
-        {
-            if (valueEscaped)
-            {
+        if (collectionName == name.trim()) {
+            if (valueEscaped) {
                 return unescape(collectionValue.trim());
             }
-            else
-            {
+            else {
                 return collectionValue;
             }
         }
@@ -95,86 +75,73 @@ function GetNameValueCollectionValue(nameValueCollection, name, valueEscaped)
 
 /* ******************* 操作Cookie ******************* */
 
-function WriteCookie(name, value)
-{
-    var today=new Date();
-    var nextYear=new Date(today.getFullYear()+1, today.getMonth(), today.getDate());
-    document.cookie=name+"="+escape(value)+";expires="+nextYear.toGMTString();
+function WriteCookie(name, value) {
+    var today = new Date();
+    var nextYear = new Date(today.getFullYear() + 1, today.getMonth(), today.getDate());
+    document.cookie = name + "=" + escape(value) + ";expires=" + nextYear.toGMTString();
 }
-function ReadCookie(name)
-{
-    var cookies=document.cookie.split(";");
+function ReadCookie(name) {
+    var cookies = document.cookie.split(";");
     return GetNameValueCollectionValue(cookies, name, true);
 }
 
 /* ******************* 时间 ******************* */
 
-function GetDate(separator)
-{
-    if (!separator)
-    {
-        separator="-";
+function GetDate(separator) {
+    if (!separator) {
+        separator = "-";
     }
-    var now=new Date();
+    var now = new Date();
     return now.getFullYear().toString()
-        +separator+(now.getMonth()+1).toString().padLeft(2, "0")
-        +separator+now.getDate().toString().padLeft(2, "0");
+        + separator + (now.getMonth() + 1).toString().padLeft(2, "0")
+        + separator + now.getDate().toString().padLeft(2, "0");
 }
-function GetTime(separator)
-{
-    if (!separator)
-    {
-        separator=":";
+function GetTime(separator) {
+    if (!separator) {
+        separator = ":";
     }
-    var now=new Date();
+    var now = new Date();
     return now.getHours().toString().padLeft(2, "0")
-        +separator+now.getMinutes().toString().padLeft(2, "0")
-        +separator+now.getSeconds().toString().padLeft(2, "0");
+        + separator + now.getMinutes().toString().padLeft(2, "0")
+        + separator + now.getSeconds().toString().padLeft(2, "0");
 }
-function GenerateTimeID()
-{
-    var now=new Date();
+function GenerateTimeID() {
+    var now = new Date();
     return now.getFullYear().toString()
-        +(now.getMonth()+1).toString().padLeft(2, "0")
-        +now.getDate().toString().padLeft(2, "0")
-        +now.getHours().toString().padLeft(2, "0")
-        +now.getMinutes().toString().padLeft(2, "0")
-        +now.getSeconds().toString().padLeft(2, "0")
-        +now.getMilliseconds().toString().padLeft(3, "0");
+        + (now.getMonth() + 1).toString().padLeft(2, "0")
+        + now.getDate().toString().padLeft(2, "0")
+        + now.getHours().toString().padLeft(2, "0")
+        + now.getMinutes().toString().padLeft(2, "0")
+        + now.getSeconds().toString().padLeft(2, "0")
+        + now.getMilliseconds().toString().padLeft(3, "0");
 }
 
 /* ******************* 注册脚本引用 ******************* */
 
-function RegisterScriptInclude(url)
-{
-    document.write(unescape("%3Cscript type=\"text/javascript\" src=\""+url+"\"%3E%3C/script%3E"));
+function RegisterScriptInclude(url) {
+    document.write(unescape("%3Cscript type=\"text/javascript\" src=\"" + url + "\"%3E%3C/script%3E"));
 }
 
 /* ******************* 格式化字符串 ******************* */
 
-function FormatString(string)
-{
-    var result=string;
-    for (var i=1;i<arguments.length;i++)
-    {
-        var pattern=new RegExp("\\{"+(i-1)+"\\}", "g");
-        result=result.replace(pattern, arguments[i].toString());
+function FormatString(string) {
+    var result = string;
+    for (var i = 1; i < arguments.length; i++) {
+        var pattern = new RegExp("\\{" + (i - 1) + "\\}", "g");
+        result = result.replace(pattern, arguments[i].toString());
     }
     return result;
 }
 
 /* ******************* 页面参数 ******************* */
 
-function ReadQueryString(name, escaped)
-{
-    var queryStringsString=window.location.toString().split("?")[1];
-    if (queryStringsString)
-    {
-        var queryStrings=queryStringsString.split("&");
+function ReadQueryString(name, escaped) {
+    var queryStringsString = window.location.toString().split("?")[1];
+    if (queryStringsString) {
+        var queryStrings = queryStringsString.split("&");
         return GetNameValueCollectionValue(queryStrings, name, escaped);
     }
-    else
-    {
+    else {
         return null;
     }
 }
@@ -185,16 +152,16 @@ function ReadQueryString(name, escaped)
 var HeadersString = "名称,今开,昨收,最新价,最高,最低,买入,卖出,成交量,成交额,买①量,买①,买②量,买②,买③量,买③,买④量,买④,买⑤量,买⑤,卖①量,卖①,卖②量,卖②,卖③量,卖③,卖④量,卖④,卖⑤量,卖⑤,日期,时间,(新浪结束符),(预留1),(预留2),(预留3),(预留4),(预留5),(预留6),(预留7),(预留8),名称,代码,涨跌,涨跌率,成本,盈亏,盈亏率,持有量,总现值,总成本,总盈亏,快链";
 
 //缺省股票数据字符串
-var DefaultStockDatasString="?,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"+GetDate()+","+GetTime();
+var DefaultStockDatasString = "?,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0," + GetDate() + "," + GetTime();
 
 //显示索引列表
 var DisplayIndexsString = "41,3,43,44,4,5,1,2,9,45,47,48,51,31,52";
 
 //着色基准索引
-var ColorBaseIndex=43;
+var ColorBaseIndex = 43;
 
 //刷新时间（毫秒）
-var RefreshInterval=5000;
+var RefreshInterval = 5000;
 
 //股票数据定义地址格式
 var StockDatasDeclareUrlFormat = "http://hq.sinajs.cn/rn={0}&list={1}";
@@ -208,14 +175,14 @@ http://60.28.2.64/rn=1114&list=sh000001,sz399001,sz399106
 http://60.28.2.65/rn=1115&list=sh000001,sz399001,sz399106
 http://60.28.2.66/rn=1116&list=sh000001,sz399001,sz399106
 http://60.28.2.67/rn=1117&list=sh000001,sz399001,sz399106
-*/ 
+*/
 
 //股票数据取回器地址格式
-var StockDatasFetcherUrlFormat="StockDatasFetcher.htm?rn={0}&list={1}";
+var StockDatasFetcherUrlFormat = "StockDatasFetcher.htm?rn={0}&list={1}";
 
 //列格式
 var StockDisplayNameFormat = "{3} <a title=\"新浪\" href=\"http://biz.finance.sina.com.cn/suggest/lookup_n.php?country=stock&q={1}\" target=\"_blank\">{2}</a>";
-var StockLinkFormat="";
-StockLinkFormat+="\n<a title=\"Google\" href=\"http://www.google.com.hk/finance?gl=cn&q={0}\" target=\"_blank\">G</a>";
-StockLinkFormat+="\n<a title=\"历史价格\" href=\"http://www.google.com.hk/finance/historical?hl=zh-CN&q={0}\" target=\"_blank\">P</a>";
-StockLinkFormat+="\n<a title=\"技术指标\" href=\"TI.htm?{1}\" target=\"_blank\">T</a>";
+var StockLinkFormat = "";
+StockLinkFormat += "\n<a title=\"Google\" href=\"http://www.google.com.hk/finance?gl=cn&q={0}\" target=\"_blank\">G</a>";
+StockLinkFormat += "\n<a title=\"历史价格\" href=\"http://www.google.com.hk/finance/historical?hl=zh-CN&q={0}\" target=\"_blank\">P</a>";
+StockLinkFormat += "\n<a title=\"技术指标\" href=\"TI.htm?{1}\" target=\"_blank\">T</a>";
