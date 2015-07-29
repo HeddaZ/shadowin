f63a37ac9376bdbdaa2f19a6534034b1709df121
Hedda Zhang <heddaz@live.com>
2015-07-29 19:49:15 +0800

    ss class

--- ShadowStock/index.html.js
+++ ShadowStock/index.html.js
@@ -1,1 +1,12 @@
-﻿
+﻿(function (window) {
+    var _shadowStock = {};
+
+    _shadowStock.config = {
+        dataSourceUrl: "http://hq.sinajs.cn/rn={0}&list={1}",
+
+    };
+
+    
+    
+    window.ShadowStock = _shadowStock;
+})(window);
